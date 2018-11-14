7fc95aef71809c60c370ad678006687f5449b7b6
Vagtsen <34607281+vagtsen@users.noreply.github.com>
2018-11-14 15:03:15 +0200

    Add files via upload

---
+++ ListDemo/ListDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            List<int> myList = new List<int>(); //είναι δυναμικό σε σχέση με τον πίνακα, όταν του βάζω κάτι δημιουργεί κατευθείαν νέα θέση
+            var random = new Random();
+            for (int i = 0; i <10; i++)
+            {
+                myList.Add(r.Next()); //να πάω στη λίστα και να προσθέσω ακεραίους με τη χρήση της random 
+            }
+
+            myList.Sort(); // sortaro τη λίστα 
+
+            foreach (int i in myList) 
+            {
+                Console.WriteLine( i);
+            }
+
+        }
+    }
+}
