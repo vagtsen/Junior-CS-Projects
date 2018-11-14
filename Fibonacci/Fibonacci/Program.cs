e6d1fb33f3dc953df569dd5f8b21e3742af82eb5
Vagtsen <34607281+vagtsen@users.noreply.github.com>
2018-11-14 15:07:44 +0200

    Add files via upload

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a number\t"); // afinei ena tab me to \t
+            int fib = int.Parse(Console.ReadLine());
+            
+            int a = 0;
+            int b = 1;
+            
+            //int number = 1;
+            for (int i = 3; i <= fib; i++)
+            {
+                int temp = a;
+                a = b;
+                b = temp + b;
+                Console.WriteLine("the required {0} fib number is {1}", i, b);
+            }
+            Console.WriteLine(b);
+
+        }
+    }
+}
