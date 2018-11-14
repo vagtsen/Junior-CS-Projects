33e92e386546568a419db61f0931c0fb00eef132
Vagtsen <34607281+vagtsen@users.noreply.github.com>
2018-11-14 15:14:01 +0200

    Add files via upload

---
+++ SQLfromConsole2/SQLfromConsole2/SQLfromConsole2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLfromConsole2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string provider = ConfigurationManager.AppSettings["provider"];
+
+            string connectionString = ConfigurationManager.AppSettings["connectionString"];
+
+            DbProviderFactory factory = DbProviderFactories.GetFactory(provider);
+
+            using (DbConnection dbconnection = factory.CreateConnection())
+            {
+                if (dbconnection == null)
+                {
+                    System.Console.WriteLine("Connection Error");
+                    return;
+                }
+
+                try
+                {
+                    dbconnection.ConnectionString = connectionString;
+                    dbconnection.Open();
+
+                    DbCommand command = factory.CreateCommand();
+                    command.Connection = dbconnection;
+                    command.CommandText = "SELECT ID, Name, Password FROM usersDetailsTable";
+
+                    using(DbDataReader dataReader = command.ExecuteReader())
+                    {
+                        while (dataReader.Read())
+                        {
+                            System.Console.WriteLine($"{dataReader["ID"]} {dataReader["Name"]} {dataReader["Password"]}");
+                        }
+                    }
+                }
+                catch(SystemException ex)
+                {
+                    System.Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    dbconnection.Close();
+                }
+            }
+        }
+    }
+}
