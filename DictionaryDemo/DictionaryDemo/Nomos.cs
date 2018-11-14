d686cdd49480cb1677e8b2ffe22803ba4ff0187b
Vagtsen <34607281+vagtsen@users.noreply.github.com>
2018-11-14 14:53:15 +0200

    Add files via upload

---
+++ DictionaryDemo/DictionaryDemo/Nomos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryDemo
+{
+    class Nomos
+    {
+        public string Capital { get; set; }
+        public int Population { get; set; }
+        public int Size { get; set; }
+
+
+        public Nomos (string capital, int population, int size)
+        {
+            Capital = capital;
+            Population = population;
+            Size = size;
+        }
+        // φτιάχνω το dictionary για να μου δίνει πληροφορίες για νομούς 
+
+        public static Dictionary<string,Nomos> GetNomoi()
+        {
+            var nomoi = new Dictionary<string, Nomos>();
+            // επαναχρησιμοποιώ τη μεταβλητή nomos για να γεμίσω το dictionary 
+            var nomos = new Nomos("Athina", 576, 334);
+            nomoi.Add("Attica", nomos);
+            nomos = new Nomos("Katerini", 80, 21);
+            nomoi.Add("Pieria", nomos);
+            nomos = new Nomos("Volos", 56, 8);
+            nomoi.Add("Magnisia", nomos);
+
+            return nomoi;
+
+       
+        }
+
+    }
+}
---
+++ DictionaryDemo/DictionaryDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Key value pairs 
+namespace DictionaryDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> capitals = new Dictionary<string, string>();
+            capitals.Add("Attica", "Athens");
+            capitals.Add("Pieria", "Katerini");
+            capitals.Add("Magnisia", "Volos");
+            capitals.Add("Imathias", "Veroia");
+
+            string capitalofAttica = capitals["Attica"];
+            Console.WriteLine("The capital of Attica is {0} ", capitalofAttica);
+            //string capitalofPieria = capitals["Katerini"];
+            //Console.WriteLine("The capital of Attica is {0} ", capitalofPieria);
+            //string capitalofMagnisia = capitals["Volos"];
+            //Console.WriteLine("The capital of Attica is {0} ", capitalofMagnisia);
+            //string capitalofImathias = capitals["Veroia"];
+            //Console.WriteLine("The capital of Attica is {0} ", capitalofImathias);
+
+            var nomoi = Nomos.GetNomoi();
+            var attiki = nomoi["Attica"];
+            Console.WriteLine("The Capital of Attica is {0}, population is {1}", attiki.Capital, attiki.Population);
+
+        }
+    }
+}
