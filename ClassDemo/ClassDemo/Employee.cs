0b51fafe821f9e8349223af57cd8f9e0e8b79dfa
Vagtsen <34607281+vagtsen@users.noreply.github.com>
2018-11-14 14:57:43 +0200

    Add files via upload

---
+++ ClassDemo/ClassDemo/Employee.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Employee
+    {
+        //Property
+        public int Age { get; set; } // pio eukolos kwdikas pou exei sxesi me ton apo katw kwdika an den thelo na peiraxo th get kai set mesa 
+        public string Name { get; set; }
+        
+        
+        //Property 
+        //private int age; //πεδίο age , backing variable ο ρόλος του είναι να κρατάει δεδομένα 
+                       // ιδιότητες της κλάσης (property)
+        //public int Age
+        //{
+        //    get { return age; }
+        //    set { age = value; }
+        //}
+
+        //Property 
+        //private string name;
+        //public string Name
+        //{
+
+        //    get { return name; }
+        //    set { name = value; }
+
+        //}
+        
+        //Μέθοδοι
+
+        //public int MyAge() //get method
+        //{
+        //    return age; //επιστρέφει έναν ακέραιο 
+        //}
+
+        //public void SetAge(int newAge) // οι συμπεριφορές της κλάσης απεικονίζονται με μεθόδους όπως π.χ οι get και set   
+        //{
+        //     age = newAge;            
+        //}
+
+
+
+    }
+}
---
+++ ClassDemo/ClassDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Peri = new Employee(); // create an instance of a class or object of a class
+            Peri.Age = 21;
+            Console.WriteLine("The age of the employee Peri is {0}", Peri.Age);
+        }
+    }
+}
