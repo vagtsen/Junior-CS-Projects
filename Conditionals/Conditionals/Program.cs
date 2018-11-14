fa86f9fa01585101d234b77e89f27631ebfb05c0
Vagtsen <34607281+vagtsen@users.noreply.github.com>
2018-11-14 15:09:55 +0200

    Add files via upload

---
+++ Conditionals/Conditionals/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            string ageAsString = Console.ReadLine();
+            int age = int.Parse(ageAsString);
+            
+
+            //Check for Voting
+            if (age >= 70)
+            {
+                Console.WriteLine("You are too old to vote"); //Steketai kai autonoma mono tou to if 
+
+            }
+            else if (age >= 18)
+            {
+                Console.WriteLine("You are an Adult");
+            }
+            else if (age < 18)
+            {
+                Console.WriteLine("You cannot vote .You are too young!");
+
+            }
+            else 
+            {
+                Console.WriteLine("Something went wrong!");//kalyptei ola ta ypoloipa conditions 
+            }
+          //End Check for Voting 
+        }
+    }
+}
